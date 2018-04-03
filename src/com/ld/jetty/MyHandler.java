4adf5504ca1430d2ef27588cb3ed8e95bac96756
ld <ld@ld>
2018-04-03 11:16:48 +0800

    sac

--- src/com/ld/jetty/MyHandler.java
+++ src/com/ld/jetty/MyHandler.java
@@ -11,6 +11,9 @@ import java.io.IOException;
 public class MyHandler extends AbstractHandler {
         public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response)
                 throws IOException, ServletException {
+        	/**
+        	 * 
+        	 */
             response.setContentType("text/html;charset=utf-8");  
             response.setStatus(HttpServletResponse.SC_OK);  
             baseRequest.setHandled(true);  
